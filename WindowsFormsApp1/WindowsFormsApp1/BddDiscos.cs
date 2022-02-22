e926dfe8262cf38ca244ee2f02024f1412c76df1
Facundo-Romani <facundoromani90@gmail.com>
2022-02-21 21:44:16 -0300

    Bdd Discos

---
+++ WindowsFormsApp1/WindowsFormsApp1/BddDiscos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    internal class BddDiscos
+    {
+        public List<Discos> listar()
+        {
+            List <Discos> lista = new List<Discos>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lectura;
+
+            try
+            {
+                conexion.ConnectionString = "server =.\\SQLEXPRESS; database = DISCOS_DB; integrated security = true; ";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select Titulo, FechaLanzamiento, CantidadCanciones from DISCOS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lectura = comando.ExecuteReader();
+
+                while(lectura.Read())
+                {
+                    Discos auxDiscos = new Discos();
+                    auxDiscos.titulo = (string)lectura["Titulo"];
+                    auxDiscos.FechaLanzamiento = lectura.GetDateTime(1);
+                    auxDiscos.CantidadDeCanciones = lectura.GetInt32(2);
+
+                    lista.Add(auxDiscos);
+                }
+                conexion.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Discos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Discos
+    {
+        public String titulo { get; set; }
+
+        public DateTime FechaLanzamiento { get; set; }
+
+        public int CantidadDeCanciones { get; set; }
+    }
+}
